2c10129f87cecf0149f6f59be96dac6891650e4c
maheshalayil <dotmahee@gmail.com>
2019-06-21 17:58:08 +0530

    1 file modified, 2 added

---
+++ Mahesh.js
@@ -0,0 +1,11 @@
+import React, { Component  } from 'react';
+
+export default class MaheshClassComponent extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return ( <h1>mahesh is cool</h1> );
+  }
+};
+
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import './style.css';
+import TestCool from './Mahesh';
+import TestComp from './MaheshFunction';
 
 class App extends Component {
   constructor() {
@@ -14,10 +15,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
         <p>
           Start editing to see some magic happen :)
         </p>
+        <c1 />
+        <TestCool />
+        <TestComp />
       </div>
     );
   }
