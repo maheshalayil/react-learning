193f1abbe879cd7dc8c5b02fe67090f7d87d2fb0
maheshalayil <dotmahee@gmail.com>
2019-06-22 14:54:17 +0530

    2 files modified, 1 added

--- MyForm.js
+++ MyForm.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import SearchField from './SearchField'
+import git from './gitapi';
 
 class MyForm extends React.Component {
+  state = { total: 0, users: []};
 
-  handleSubmit = (term) => {
-    console.log(`from parent ${term}`);
+  handleSubmit = async (term) => {
+    var response = await git.get('/search/users', {
+      params: { q: term }
+    });
+    this.setState({ 
+        total: response.data.total_count, 
+        users: response.data.items 
+      });
   }
-  
+
   render() {
     return (
       <div>
         <h1>form example </h1>
         <SearchField onSubmit={this.handleSubmit} />
+        <p>Found {this.state.total} records</p>
       </div>
     )
   }
